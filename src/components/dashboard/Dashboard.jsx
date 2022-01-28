691985138ad8339a2999e2c3ba5f1a51e20a85a2
PABLO <pablo.d.guzman.1@gmail.com>
2022-01-28 14:15:48 -0300

    primer commit

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import ApiUsers from '../users/ApiUsers';
 
 export default function Dashboard() {
   return (
       <div>
         <h1>Prueba de DASHBORAD.jsx</h1>
+
+        <ApiUsers />
       </div>
     //console.log('ApiProductsId')
   )
